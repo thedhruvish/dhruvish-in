a4d57e0d185f37976050ca493f897e29eda8904b
thedhruvish <thedhruvish@gmail.com>
2025-10-30 10:18:45 +0530

    add the analystics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import { generateMetadata } from "@/config/Meta";
 import { ViewTransitions } from "next-view-transitions-react-19";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import Clarity from "@microsoft/clarity";
 
 export const metadata = generateMetadata("/");
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProviders";
+
+Clarity.init(process.env.NEXT_PUBLIC_MS_CLARITY!);
 
 export default function RootLayout({
   children,
@@ -23,6 +27,8 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             {children}
+
+            <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS!} />
           </ThemeProvider>
         </body>
       </html>
